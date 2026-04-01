fc491172b97621f21f233114fb19f6b4801d11a7
haiderappguys <haider.a@theappguys.com>
2026-04-01 10:08:01 +0000

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,8 @@ export default defineConfig(({ mode }) => {
     },
     preview: {
       host: '0.0.0.0',
-      port: 8080
+      port: 8080,
+      allowedHosts: ['knovatwin.com']
     }
   }
 })
